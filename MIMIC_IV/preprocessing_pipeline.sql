5e1197e98e0bb15bc6eb83edb80b7cea631819c5
alz21 <agathezecevic@gmail.com>
2022-05-27 14:46:40 +0100

    added mortality

--- MIMIC_IV/preprocessing_pipeline.sql
+++ MIMIC_IV/preprocessing_pipeline.sql
@@ -120,11 +120,11 @@ SELECT * FROM mimiciv.preprocessed order by icustay_id, hour_from_intime;
 
 */
 ---Get demographic table 
-SELECT DISTINCT v.stay_id, p.gender, p.anchor_age AS age, ROUND(hw.weight_first / POWER(hw.height_first / 100, 2), 3) AS BMI
+SELECT DISTINCT v.stay_id, p.gender, p.anchor_age AS age, ROUND(hw.weight_first / POWER(hw.height_first / 100, 2), 3) AS BMI, case when p.dod is not null and p.dod <= icu.outtime then 1 else 0 end as death,
 FROM mimiciv.aggregated_vitals v
 JOIN mimic_icu.icustays icu ON v.stay_id = icu.stay_id
 JOIN mimic_core.patients p ON p.subject_id = icu.subject_id
-JOIN mimiciv.heightweight hw ON hw.stay_id = icu.stay_id
+LEFT JOIN mimiciv.heightweight hw ON hw.stay_id = icu.stay_id
 
 ORDER BY stay_id ;
 
